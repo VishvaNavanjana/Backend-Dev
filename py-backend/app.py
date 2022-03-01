648f3c42b036091adb83f9b0804d31d60ad8ff8a
thisaramanohara <thisaramanohara@gmail.com>
2022-03-02 00:16:26 +0530

    Iris flower problem intergrated

--- py-backend/app.py
+++ py-backend/app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, request
 import random
+import joblib
 
 app = Flask(__name__)
+
+model = joblib.load('iris-flower-KNN.sav')
+flowers = {0: "setosa", 1: "versicolor", 2: "virginica"}
 
 @app.route('/')
 def testPYBackend():
@@ -10,3 +14,14 @@ def testPYBackend():
 @app.route('/getRandomNumber')
 def serveRandomNumber():
     return str(random.randint(0, 100))
+
+@app.route('/flower', methods=["POST"])
+def flower():
+    input_json = request.get_json(force=True)
+    custom_data=[]
+    for _, val in input_json.items():
+        custom_data.append(val)
+
+    result = model.predict([custom_data])
+
+    return flowers[result[0]]
