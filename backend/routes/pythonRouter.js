c433af7ae26f6cc28bf4a4a94baf32ede4e491a9
thisaramanohara <thisaramanohara@gmail.com>
2022-03-08 18:21:00 +0530

    Get and Post fixed using axios

--- backend/routes/pythonRouter.js
+++ backend/routes/pythonRouter.js
@@ -21,36 +21,13 @@ pythonRouter.route('/')
         console.log(responseFromPython.data);
         res.send({number: responseFromPython.data})
     })
-
-    // const options = {
-    //     url: 'http://127.0.0.1:5000/getRandomNumber',
-    //     method: 'GET'
-    // };
-
-    // request(options, (error, response, body) => {
-    //     console.error('error:', error); // Print the error
-    //     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    //     console.log('body:', body); // Print the data received
-    //     res.send(body); //Display the response on the website
-    // });
 })
 .post((req,res,next) => {
+    axios.post('http://127.0.0.1:5000/flower', req.body)
+    .then((responseFromPython) => {
+        res.send({prediction: responseFromPython.data});
+    });
 
-    // console.log(req.body);
-
-    // // request to "/flower" endpoint in python backend
-    // const options = {
-    //     url: 'http://127.0.0.1:5000/flower',
-    //     json: true,
-    //     body: req.body
-    // };
- 
-    // request.post(options, (error, response, body) => {
-    //     console.error('error:', error); // Print the error
-    //     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    //     console.log('body:', body); // Print the data received
-    //     res.send(body); //Display the response on the website
-    // });
 })
 .put((req,res,next) => {
     res.statusCode = 403;
