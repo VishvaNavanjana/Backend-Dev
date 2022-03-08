5fe9fa1028e556cb9cfea2e55420c6aadc14e610
thisaramanohara <thisaramanohara@gmail.com>
2022-03-08 18:13:00 +0530

    Get from axios is done

--- backend/routes/pythonRouter.js
+++ backend/routes/pythonRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const request = require('request');
+// const request = require('request');
+const axios = require('axios');
 
 const pythonRouter = express.Router();
 
@@ -15,35 +16,41 @@ pythonRouter.route('/')
 })
 .get((req,res,next) => {
 
-    const options = {
-        url: 'http://127.0.0.1:5000/getRandomNumber',
-        method: 'GET'
-    };
+    axios.get('http://127.0.0.1:5000/getRandomNumber')
+    .then((responseFromPython) => {
+        console.log(responseFromPython.data);
+        res.send({number: responseFromPython.data})
+    })
 
-    request(options, (error, response, body) => {
-        console.error('error:', error); // Print the error
-        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        console.log('body:', body); // Print the data received
-        res.send(body); //Display the response on the website
-    });
+    // const options = {
+    //     url: 'http://127.0.0.1:5000/getRandomNumber',
+    //     method: 'GET'
+    // };
+
+    // request(options, (error, response, body) => {
+    //     console.error('error:', error); // Print the error
+    //     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    //     console.log('body:', body); // Print the data received
+    //     res.send(body); //Display the response on the website
+    // });
 })
 .post((req,res,next) => {
 
-    console.log(req.body);
+    // console.log(req.body);
 
-    // request to "/flower" endpoint in python backend
-    const options = {
-        url: 'http://127.0.0.1:5000/flower',
-        json: true,
-        body: req.body
-    };
+    // // request to "/flower" endpoint in python backend
+    // const options = {
+    //     url: 'http://127.0.0.1:5000/flower',
+    //     json: true,
+    //     body: req.body
+    // };
  
-    request.post(options, (error, response, body) => {
-        console.error('error:', error); // Print the error
-        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        console.log('body:', body); // Print the data received
-        res.send(body); //Display the response on the website
-    });
+    // request.post(options, (error, response, body) => {
+    //     console.error('error:', error); // Print the error
+    //     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    //     console.log('body:', body); // Print the data received
+    //     res.send(body); //Display the response on the website
+    // });
 })
 .put((req,res,next) => {
     res.statusCode = 403;
